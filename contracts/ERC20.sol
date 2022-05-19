3ccc805d403ec16fc0cdc2969050c5a04916a21f
Marco Menozzi <marco.menozzi@consensys.net>
2022-05-19 11:46:00 +0100

    chore(): initial natspec for erc20

--- contracts/ERC20.sol
+++ contracts/ERC20.sol
@@ -1,24 +1,52 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 import {ERC20Proxy} from "./tokens/proxy/ERC20/ERC20Proxy.sol";
 import {RolesBase} from "./utils/roles/RolesBase.sol";
 
+/**
+ * @title ERC20 extendable token
+ * @author Eddie Penta
+ * @notice You can use this contract to deploy an extendable ERC20 token
+ * @dev All inherited functions are called by using the convention: ContractName.functionToCall()
+ */
 contract ERC20 is ERC20Proxy {
     uint256 public initalSupply;
-    
+
+    /**
+     * @dev Constructor takes the ERC20 logic contract address, important for proper functioning
+     * @param name_ The name of the token
+     * @param symbol_ The symbol of the token
+     * @param symbol_ The symbol of the token
+     */
     constructor(
-        string memory name_, string memory symbol_, bool allowMint, 
-        bool allowBurn, address owner, uint256 _initalSupply,
-        uint256 maxSupply, address logicAddress
-    ) ERC20Proxy(name_, symbol_, allowMint, allowBurn, owner, maxSupply, logicAddress) {
+        string memory name_,
+        string memory symbol_,
+        bool allowMint,
+        bool allowBurn,
+        address owner,
+        uint256 _initalSupply,
+        uint256 maxSupply,
+        address logicAddress
+    )
+        ERC20Proxy(
+            name_,
+            symbol_,
+            allowMint,
+            allowBurn,
+            owner,
+            maxSupply,
+            logicAddress
+        )
+    {
         initalSupply = _initalSupply;
 
         if (owner != _msgSender()) {
             //Temporaroly add minter role to msg.sender
             //If the sender is not the final owner
-            RolesBase._addRole(_msgSender(), TOKEN_MINTER_ROLE); 
+            RolesBase._addRole(_msgSender(), TOKEN_MINTER_ROLE);
         }
-        
+
         ERC20Proxy._mint(owner, initalSupply);
 
         if (owner != _msgSender()) {
