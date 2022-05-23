4c1f0e8d2a30163710429f894e78e915c4f90b11
Eddie Penta <ecp4224@gmail.com>
2022-05-23 10:37:27 -0400

    Update contracts/extensions/ExtensionProxy.sol

--- contracts/extensions/ExtensionProxy.sol
+++ contracts/extensions/ExtensionProxy.sol
@@ -179,7 +179,7 @@ contract ExtensionProxy is IExtensionMetadata, ExtensionBase {
 
     /**
      * @notice An array of function signatures this extension adds when
-     * registered when a TokenProxy
+     * registered with a TokenProxy
      * @dev This function is used by the TokenProxy to determine what
      * function selectors to add to the TokenProxy
      */
